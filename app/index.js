207914e684d903fbc94ace3b6f9f270171fd8160
Anna Jancso <anna.t.jancso@gmail.com>
2020-04-26 22:47:14 +0200

    Stream mp4 videos

--- app/index.js
+++ app/index.js
@@ -51,6 +51,47 @@ app.get('/videos/:videoId/', function (req, res) {
     res.render('video.html', {'video': video});
 });
 
+// https://github.com/daspinola/video-stream-sample
+app.get('/videos/:videoId/download/', function(req, res) {
+    const videoPath = path.join(videos_dir, req.params['videoId']);
+    console.log(videoPath);
+    const stat = fs.statSync(videoPath);
+    const fileSize = stat.size;
+    const range = req.headers.range;
+
+    if (range) {
+        const parts = range.replace(/bytes=/, "").split("-");
+        const start = parseInt(parts[0], 10);
+        const end = parts[1]
+            ? parseInt(parts[1], 10)
+            : fileSize-1;
+
+        if(start >= fileSize) {
+            res.status(416).send('Requested range not satisfiable\n'+start+' >= '+fileSize);
+            return
+        }
+
+        const chunksize = (end-start)+1;
+        const file = fs.createReadStream(videoPath, {start, end});
+        const head = {
+            'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': 'video/mp4',
+        };
+
+        res.writeHead(206, head);
+        file.pipe(res)
+    } else {
+        const head = {
+            'Content-Length': fileSize,
+            'Content-Type': 'video/mp4',
+        };
+        res.writeHead(200, head);
+        fs.createReadStream(videoPath).pipe(res)
+    }
+});
+
 
 app.get('/webcam/', (req, res) => {
     res.render('webcam.html');
